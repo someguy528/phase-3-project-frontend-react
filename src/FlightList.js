ebf0aa50a70163267f8e52ab45861a00c2fa9de4
Brandon Castro <castroyou999@gmail.com>
2023-01-31 13:21:31 -0500

    moved passengers trivia to passengerslist component

--- src/FlightList.js
+++ src/FlightList.js
@@ -6,44 +6,19 @@ import { Switch, Route, useRouteMatch } from "react-router-dom"
 import BookingDetail from "./BookingDetail"
 import BookingDetailEdit from "./BookingDetailEdit"
 import BookingNew from "./BookingNew"
-import { useState } from "react"
 
 function FlightList({flights, onFlightAdd, onFlightEdit, onFlightDelete, onBookingChange, onBookingDelete, onBookingAdd}){
-    const [mostBookings, setMostBookings] = useState("Whats the Most Bookings for a passenger?")
-    const [mostPassengers, setMostPassengers] = useState("Who has the Most Bookings?")
-
 
     const route = useRouteMatch().url
     const allFlights = flights.map(flight => {
         return ( <FlightListing flight={flight} key={flight.id} />  )
     })
 
-    function handleBookingMostClick(){
-        fetch("http://localhost:9292/bookings/most")
-        .then(resp=>resp.json())
-        .then(data => {
-            const mostBookings = data[0][1]
-            setMostBookings(`The Most bookings for a passenger: ${mostBookings}`)
-        })
-    }
-    function handleBookingPassengersMostClick(){
-        fetch("http://localhost:9292/bookings/most_bookings_passengers")
-        .then(resp=>resp.json())
-        .then(data => {
-            const mostBookingPassengers = data.map(data=> data.name).toString()
-            setMostPassengers(`The passengers with the most bookings: ${mostBookingPassengers}`)
-        })
-    }
-
     return (
         <div>
             <h1>Flights</h1>
             <Switch> 
                 <Route exact path={`${route}`} >
-                    <h3>{mostBookings}</h3>
-                    <button onClick={handleBookingMostClick} > Click </button>
-                    <h3>{mostPassengers}</h3>
-                    <button onClick={handleBookingPassengersMostClick} > Click </button>
                     {allFlights}
                 </Route>
                 <Route exact path={`${route}/new`} >
--- src/PassengersList.js
+++ src/PassengersList.js
@@ -15,6 +15,26 @@ function PassengersList(){
         })
     },[])
     const route = useRouteMatch().url
+
+    const [mostBookings, setMostBookings] = useState("Whats the Most Bookings for a passenger?")
+    const [mostPassengers, setMostPassengers] = useState("Who has the Most Bookings?")
+
+    function handleBookingMostClick(){
+        fetch("http://localhost:9292/bookings/most")
+        .then(resp=>resp.json())
+        .then(data => {
+            const mostBookings = data[0][1]
+            setMostBookings(`The Most bookings for a passenger: ${mostBookings}`)
+        })
+    }
+    function handleBookingPassengersMostClick(){
+        fetch("http://localhost:9292/bookings/most_bookings_passengers")
+        .then(resp=>resp.json())
+        .then(data => {
+            const mostBookingPassengers = data.map(data=> data.name).toString()
+            setMostPassengers(`The passengers with the most bookings: ${mostBookingPassengers}`)
+        })
+    }
     
     if(!isPassengersLoaded) return <h1>Loading Passengers...</h1>
 
@@ -30,7 +50,12 @@ function PassengersList(){
             </Route>
             <Route path={`${route}`}>
                 <section>
+
                     <h2> Passengers List </h2>
+                    <h3>{mostBookings}</h3>
+                    <button onClick={handleBookingMostClick} > Click </button>
+                    <h3>{mostPassengers}</h3>
+                    <button onClick={handleBookingPassengersMostClick} > Click </button>
                     {allPassengers}
                 </section>
             </Route>
